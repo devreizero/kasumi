34fbc8273ec38d1cd575f5497bd12d2480c9d245
devreizero <gmreizero@gmail.com>
2025-12-04 12:03:00 +0700

    modify: linker.ld.in and macros
    Replaced *.boot to *.init because it sounds better.
    Added rounding/aligning macros.
    Added __kernelStart and __kernelEnd to locate the
    kernel address.

--- kernel/include/common/macros.h
+++ kernel/include/common/macros.h
@@ -1,10 +1,17 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#define __rounddown(x, align) ((x) & ~((align) - 1))
+#define __roundup(x, align) (((x) + (align) - 1) & ~((align) - 1))
+
+#define __aligndown(x, align) __rounddown(x, align)
+#define __alignup(x, align) __roundup(x, align)
+
 #define __noreturn __attribute__((noreturn))
 #define __packed __attribute__((packed))
 #define __naked __attribute__((naked))
 #define __naked_section(sec) __attribute__((naked, section(sec)))
-#define __bootdata __attribute__((section(".data.boot")))
+#define __initdata __attribute__((section(".data.init")))
+#define __init __attribute__((section(".text.init")))
 
 #endif
