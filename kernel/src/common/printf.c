8f4f5df67e693dd6f43d963856278678d9b8ee11
devreizero <gmreizero@gmail.com>
2025-12-09 14:42:54 +0700

    add: more formatting to vsprintf
    Hey friends! Finally after all those times using just
    %lx and %lu, now we have %u, %p, and %zu too!
    Also added padding right, though not padding
    left yet!

--- kernel/src/common/printf.c
+++ kernel/src/common/printf.c
@@ -3,6 +3,7 @@
 #include <serial.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 
 static void reverse(char *str, size_t len) {
   size_t i = 0, j = len - 1;
@@ -30,8 +31,21 @@ static size_t ulong_to_str(unsigned long val, char *buf, int base) {
   return i;
 }
 
+static unsigned long strToUlong(const char *s, int len) {
+  unsigned long value = 0;
+
+  for (int i = 0; i < len; i++) {
+      value = value * 10 + (s[i] - '0');
+  }
+
+  return value;
+}
+
 int vsprintf(char *out, const char *fmt, va_list args) {
+  unsigned long val;
   char *buf = out;
+  int width;
+  uint8_t base, useLong;
   while (*fmt) {
     if (*fmt != '%') {
       if (*fmt == '\n')
@@ -42,13 +56,22 @@ int vsprintf(char *out, const char *fmt, va_list args) {
     fmt++; // skip '%'
 
     // Width for %*s
-    int width = 0;
+    width = 0;
     if (*fmt == '*') {
       width = va_arg(args, int);
       fmt++;
+    } else if (*fmt >= '0' && *fmt <= '9') {
+      char tmp[32];
+      int i = 0;
+      do {
+        tmp[i++] = *fmt++;
+      } while(*fmt >= '0' && *fmt <= '9' && i < 32);
+      width = strToUlong(tmp, i);
     }
 
-    switch (*fmt) {
+    useLong = 0;
+    base = 10;
+    start: switch (*fmt) {
     case 's': {
       const char *s = va_arg(args, const char *);
       int len = 0;
@@ -59,41 +82,73 @@ int vsprintf(char *out, const char *fmt, va_list args) {
         *buf++ = ' ';
       while (*s)
         *buf++ = *s++;
-      break;
+      goto endIncremented;
     }
     case 'c': {
       char c = (char)va_arg(args, int);
       *buf++ = c;
-      break;
+      goto endIncremented;
     }
     case 'l': {
       fmt++;
-      unsigned long val = va_arg(args, unsigned long);
-      char tmp[32];
-      size_t len = 0;
-      if (*fmt == 'u') {
-        len = ulong_to_str(val, tmp, 10);
-      } else if (*fmt == 'x') {
-        len = ulong_to_str(val, tmp, 16);
+      useLong = 1;
+      goto start;
+    }
+    case 'p': {
+      uintptr_t val = va_arg(args, uintptr_t);
+      if(val == 0) {
+        *buf++ = 'n';
+        *buf++ = 'u';
+        *buf++ = 'l';
+        *buf++ = 'l';
+        goto endIncremented;
+      } else {
+        *buf++ = '0';
+        *buf++ = 'x';
+        fmt++;
+        goto number;
       }
-      for (size_t i = 0; i < len; i++)
-        *buf++ = tmp[i];
-      break;
     }
-    case 'x': {
-      unsigned int val = va_arg(args, unsigned int);
-      char tmp[32];
-      size_t len = ulong_to_str(val, tmp, 16);
-      for (size_t i = 0; i < len; i++)
-        *buf++ = tmp[i];
-      break;
+    case 'z': {
+      if (*++fmt != 'u') {
+        *buf++ = '%';
+        *buf++ = 'z';
+        *buf++ = *fmt;
+        goto end;
+      }
+
+      val = va_arg(args, size_t);
+      goto format;
     }
+    case 'x': base = 16; goto number;
+    case 'u': base = 10; goto number;
     default:
+      if (*fmt != '%') *buf++ = '%';
       *buf++ = *fmt;
-      break;
+      goto endIncremented;
     }
+
+    number:
+      val = useLong ? va_arg(args, unsigned long) : va_arg(args, unsigned int);
+    
+    format: { 
+      char tmp[32];
+      size_t len = ulong_to_str(val, tmp, base);
+      
+      int pad = width - len;
+      while (pad-- > 0)
+        *buf++ = ' ';
+      
+      for (size_t i = 0; i < len; i++)
+        *buf++ = tmp[i];
+    }
+
+    endIncremented:
     fmt++;
+
+    end: {}
   }
+
   *buf = '\0';
   return buf - out;
 }
